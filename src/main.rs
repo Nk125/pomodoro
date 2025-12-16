333ef25725f8e9e1d8162e2e765e1b62dea4ccff
Nekes <nekes@riseup.net>
2025-12-16 17:35:35 -0600

    feat: Add logging/debug with iced v0.14
    Adds logging to the app and updates iced to v0.14 for debugging support

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
+use env_logger::Env;
 use pomodoro::app;
 
 fn main() -> iced::Result {
-    iced::application("Pomodoro", app::Pomodoro::update, app::Pomodoro::view)
-        .centered()
-        .run()
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
+    iced::application(
+        app::Pomodoro::default,
+        app::Pomodoro::update,
+        app::Pomodoro::view,
+    )
+    .antialiasing(true)
+    .run()
 }
